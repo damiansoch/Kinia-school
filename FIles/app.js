3da39ec6bc997b4031172e06e868b33ba23ca9a2
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-01-31 07:25:32 +0000

    app1 prepared

--- FIles/app.js
+++ FIles/app.js
@@ -1,9 +1,49 @@
-//incluce at the page load
-$('#includedContent').load('./shihTzu/shihTzuBreed.html');
+//appointment 1
+$('#breed').fadeIn('slow');
 
 //include on click
-const clickHandler = (e) => {
-  $(function () {
-    $('#includedContent').load(e);
+const clickHandlerBreed = () => {
+  console.log('object');
+  $('#plan').fadeOut('slow');
+  $('#summary').fadeOut('slow');
+  $('#steps').fadeOut('slow');
+  $('#details').fadeOut('slow', function () {
+    $('#breed').fadeIn('slow');
+  });
+};
+const clickHandlerDetails = () => {
+  console.log('object');
+  $('#plan').fadeOut('slow');
+  $('#summary').fadeOut('slow');
+  $('#steps').fadeOut('slow');
+  $('#breed').fadeOut('slow', function () {
+    $('#details').fadeIn('slow');
+  });
+};
+const clickHandlerPlan = () => {
+  console.log('object');
+  $('#details').fadeOut('slow');
+  $('#summary').fadeOut('slow');
+  $('#steps').fadeOut('slow');
+  $('#breed').fadeOut('slow', function () {
+    $('#plan').fadeIn('slow');
+  });
+};
+const clickHandlerSteps = () => {
+  console.log('object');
+  $('#details').fadeOut('slow');
+  $('#summary').fadeOut('slow');
+  $('#plan').fadeOut('slow');
+  $('#breed').fadeOut('slow', function () {
+    $('#steps').fadeIn('slow');
+  });
+};
+const clickHandlerSummary = () => {
+  console.log('object');
+  $('#details').fadeOut('slow');
+  $('#plan').fadeOut('slow');
+  $('#steps').fadeOut('slow');
+  $('#breed').fadeOut('slow', function () {
+    $('#summary').fadeIn('slow');
   });
 };
